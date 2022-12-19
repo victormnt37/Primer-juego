cd42f8cd7a2e4a223e6bd35a782ba31950e746d5
victormnt37 <90858109+victormnt37@users.noreply.github.com>
2022-12-19 17:58:23 +0100

    camera shooting shenanigas

--- PrimerJuego/Assets/Scripts/PointAndShoot.cs
+++ PrimerJuego/Assets/Scripts/PointAndShoot.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PointAndShoot : MonoBehaviour
 {
     public Vector3 target;
-    public float shootingHeight = 0; //this variable helps calculate the height of the bullet on the screen, taking reference the camera height
+    public float shootingHeight = 3; //this variable helps calculate the height of the bullet on the screen, taking reference the camera height
     public GameObject crosshairs;
     public Vector3 mousePos;
 
@@ -18,9 +18,10 @@ public class PointAndShoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        target = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
+        target = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, transform.position.y - shootingHeight, Input.mousePosition.z));
         mousePos = Input.mousePosition;
-        Debug.Log(target);
-        crosshairs.transform.position = new Vector2(target.x, target.y);
+        Debug.Log("target" + target);
+        crosshairs.transform.position = new Vector2(target.x, target.z);
+        Debug.Log(crosshairs.transform.position);
     }
 }
